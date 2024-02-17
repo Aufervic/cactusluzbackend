147f9778016aabf80cef5b516dd745b2affb6609
Aufervic <aufer.vicc@gmail.com>
2024-02-17 02:32:16 +0000

    Cambio minimo, de prueba

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 // initializations
-const app = express()
+const app = express();
 
 // middlewares
-app.use(morgan('dev'))
-app.use(cors({origin: "*"}))
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors({ origin: "*" }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // routes
 app.get("/", (req, res) => {
-    res.send("Backend CactusLuz")
-})
+  res.send("Backend CactusLuz :3");
+});
 
-module.exports = app
+module.exports = app;
