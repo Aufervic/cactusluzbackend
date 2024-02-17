59a6829307573190f12b16a05c669ee110537c59
Aufer Victoriano <aufer.vicc@gmail.com>
2024-02-16 21:04:14 -0500

    Backend: server básico creado.

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+
+// initializations
+const app = express()
+
+// middlewares
+app.use(morgan('dev'))
+app.use(cors({origin: "*"}))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// routes
+app.get("/", (req, res) => {
+    res.send("Backend CactusLuz")
+})
+
+module.exports = app
